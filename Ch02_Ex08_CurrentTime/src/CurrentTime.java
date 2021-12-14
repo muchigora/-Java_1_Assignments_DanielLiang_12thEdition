2cbe1c8a22ee706e74d24974e57644828c6dd5a7
Muchi Gora <muchigora@gmail.com>
2021-12-14 09:52:28 -0500

    first commit

---
+++ Ch02_Ex08_CurrentTime/src/CurrentTime.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**Ch02 Ex08: Current Time
+ * This program displays current time in GMT. The user may enter a timezone offset to GMT, 
+ * and the program will display the time in the specified Time Zone
+ * @author Muchi compiled June 29th of 2021
+ *
+ */
+public class CurrentTime {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
