422d6d4e429aaf20ad914b89408688874d97a7cc
Muchi Gora <muchigora@gmail.com>
2021-12-14 09:30:58 -0500

    first commit

---
+++ Ch01_Ex05_SummationOfASeries/src/SummationOfASeries.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Muchi
+ *
+ */
+public class SummationOfASeries {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		x = (1+2+3+4+5+6+7+8+9);
+		System.out.println(x);		
+		
+
+	}
+
+}
