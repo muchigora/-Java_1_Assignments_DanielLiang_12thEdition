f93d1415c4f11529c10fe6b1ab6626eb48b66be9
Muchi Gora <muchigora@gmail.com>
2021-12-14 09:41:23 -0500

    first commit

---
+++ Ch01_Ex08_AreaAndPerimeterOfACircle/src/AreaAndPerimeterOfACircle.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**Chapter1 Exercise 8: Area & Perimeter of a Circle
+ * This Program calculates Area and Perimeter of a circle
+ * @author Muchi
+ *
+ */
+public class AreaAndPerimeterOfACircle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 5.5;
+		double area = radius * radius * Math.PI;
+		double perimeter = 2 * radius * Math.PI;
+		
+		System.out.println(area);
+		System.out.println(perimeter);
+
+	}
+
+}
---
+++ Ch01_Ex09_Rectangle/src/AreaAndPerimeter.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**Chapter 1 Exercise 9: Area and Perimeter of a Rectangle
+ * This Program Calculates Area and Perimeter of a Rectangle
+ * 
+ * @author Muchi compiled on Jun 27th of 2021
+ *
+ */
+public class AreaAndPerimeter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Defining Variable
+		double width = 4.5;
+		double height = 7.9;
+		
+		//Computations
+		double area = width * height;
+		
+		double perimeter = (2 * width) + (2 * height);
+		
+		System.out.println(area);
+		System.out.println(perimeter);
+
+	}
+
+}
