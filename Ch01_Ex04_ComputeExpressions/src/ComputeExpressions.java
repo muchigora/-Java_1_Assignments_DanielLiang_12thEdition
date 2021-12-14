f0e8923bd1bf191e2bc8c7a0fc7e3af32a47d402
Muchi Gora <muchigora@gmail.com>
2021-12-14 09:28:57 -0500

    first commit

---
+++ Ch01_Ex04_ComputeExpressions/src/ComputeExpressions.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**Chapter 1 Exercise 1 - This Program Computes a Mathematical Expression
+ * @author Muchi Jun 26th, 2021
+ *
+ */
+public class ComputeExpressions {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = (9.5*4.5-2.5*3)/(45.5-3.5);
+		System.out.println(x);
+		
+		double y = ((9.5*4.5)-(2.5*3))/(45.5-3.5);
+		System.out.println(y);
+
+	}
+
+}
