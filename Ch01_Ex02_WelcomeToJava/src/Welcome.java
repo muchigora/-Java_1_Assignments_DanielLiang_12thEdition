7221b5b1e6c6a09cc8281eb35ac6837af1826005
Muchi Gora <muchigora@gmail.com>
2021-12-14 09:26:38 -0500

    First Commit

---
+++ Ch01_Ex02_WelcomeToJava/src/Welcome.java
@@ -0,0 +1,19 @@
+/*
+ * Welcome to Java
+ * This program displays the message 'Welcome to Java' Five Times
+ * @author Muchi Jun 25th, 2021
+ * */
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "Welcome to Java";
+		
+		for (int i = 0; i<5; i++) {
+			System.out.println(message);
+		}
+		
+
+	}
+
+}
