c407346a78fc7dadcc1d8f8da035d1e1b47d330a
Muchi Gora <muchigora@gmail.com>
2021-12-14 10:14:10 -0500

    first commit

---
+++ Ch09_Ex03_DateObject/src/dateObject/DateObject.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package dateObject;
+
+/**
+ * 
+ * *9.3 (Use the Date class) 
+ * Write a program that creates a Date object, sets its elapsed time to 10,000 , 
+ * 100,000 , 1,000,000 , 10,000,000 , 100,000,000 , 1,000,000,000 , 10'000,000,000 , 
+ * and 100.000,000,000 , and displays the date and time using the toString() method, respectively.
+ * @author Muchi
+ *
+ */
+public class DateObject {
+
+}
