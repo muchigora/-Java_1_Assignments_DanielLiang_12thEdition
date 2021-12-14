7ba57f6b51e045f334c7a71f15279de09e6c6127
Muchi Gora <muchigora@gmail.com>
2021-12-14 16:05:16 -0500

    First Commit

---
+++ Ch03_Ex12_PalindromeInteger/src/palindrome/integer/PalindromeInteger.java
@@ -0,0 +1,21 @@
+package palindrome.integer;
+
+/**Ch03 Ex11: Palindrome integer
+ * Write a program that prompts the user to enter a three-digit integer and determines 
+ * whether it is a palindrome integer. An integer is palindrome if it reads the same from 
+ * right to left and from left to right. A negative integer is treated the same as a 
+ * positive integer. Here are sample runs of this program:
+ * -> Enter a three-digit integer: 121 result: 121 is a palindrome
+ * -> Enter a three-digit integer: 123 result: 123 is not a palindrome
+ * @author Muchi
+ *
+ */
+
+public class PalindromeInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
