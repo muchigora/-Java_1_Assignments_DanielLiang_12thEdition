33d7c1741999748d697fecbc86bf6941666e6573
Muchi Gora <muchigora@gmail.com>
2021-12-14 20:09:34 -0500

    first commit

---
+++ Ch03_Ex16_RandomPoint/src/random/rectangle/coordinates/RandomPoint.java
@@ -0,0 +1,19 @@
+package random.rectangle.coordinates;
+
+/**
+ * Ch03_Ex16: Random point
+ * Write a program that displays a random coordinate in a rectangle. 
+ * The rectangle is centered at (0, 0) with width 100 and height 200.
+ * 
+ * @author Muchi 14th Day of Dec, 2021
+ *
+ */
+
+public class RandomPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
