68405ec179509869d2c6997129024b075f439544
Muchi Gora <muchigora@gmail.com>
2021-12-14 19:53:22 -0500

    First Commit

---
+++ Ch03_Ex14_GameHeadOrTails/src/games/HeadsOrTails.java
@@ -0,0 +1,19 @@
+package games;
+
+/**
+ * Ch03 Ex 14: Game: heads or tails
+ *  Write a program that lets the user guess whether the flip of a coin results 
+ *  in heads or tails. The program randomly generates an integer 0 or 1, which represents 
+ *  head or tail. The program prompts the user to enter a guess, and reports whether the 
+ *  guess is correct or incorrect.
+ * @author Muchi Dec 14th of 2021
+ *
+ */
+public class HeadsOrTails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
