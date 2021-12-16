40a5e6f56e52949582c2e897b16d9d19ac7cd636
Muchi Gora <muchigora@gmail.com>
2021-12-16 10:54:40 -0500

    commit changes

--- Ch03_Ex14_GameHeadOrTails/src/games/HeadsOrTails.java
+++ Ch03_Ex14_GameHeadOrTails/src/games/HeadsOrTails.java
@@ -1,5 +1,7 @@
 package games;
 
+import java.util.Scanner;
+
 /**
  * Ch03 Ex 14: Game: heads or tails
  *  Write a program that lets the user guess whether the flip of a coin results 
@@ -12,8 +14,34 @@ package games;
 public class HeadsOrTails {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Create Scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Generate coin flip
+		int coinFlip = (int)(Math.random()*2);
+		
+		
+		//Guess coin side
+		System.out.println("Guess Heads or Tails?");
+		int guess = input.nextInt();
+		
+		
+		//Check guess vs coin flip
+		if(coinFlip == guess) {
+			System.out.println("Correct!");
+			if(coinFlip == 1) {
+				System.out.println("You guessed Heads");
+			}else {
+				System.out.println("You guessed Tails");
+			}
+		}else {
+			System.out.println("Wrong");
+			if(coinFlip == 1) {
+				System.out.println("You guessed Head");
+			}else {
+				System.out.println("You guessed Tails");
+			}
+		}
 	}
 
 }
