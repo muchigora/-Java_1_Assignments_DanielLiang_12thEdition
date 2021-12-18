6f9d726f4d821655bb6841b986d70cd7c8a47fc2
Muchi Gora <muchigora@gmail.com>
2021-12-18 09:36:07 -0500

    creaed class

---
+++ Ch03_Ex21_DayOfTheWeek/src/science/calendar/WeekDay.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package science.calendar;
+
+/**Ch03 Ex21 Science: day of the week
+ * See Supplied image for the question
+ * 
+ * Zeller’s congruence is an algorithm developed by Christian Zeller to calculate the day of the week. 
+ * The formula is (see supplied image of the question)
+ * where:
+ * h is the day of the week (0: Saturday, 1: Sunday, 2: Monday, 3: Tuesday, 4: Wednesday, 5: Thursday, and 6: Friday).
+ * q is the day of the month. 
+ * m is the month (3: March, 4: April, ..., 12: December). January and February are counted as months 13 and 14 of the previous year.
+ * j is year 100 
+ * k is the year of the century (i.e., year % 100). 
+ * 
+ * Note all divisions in this exercise perform an integer division. Write a program that prompts 
+ * the user to enter a year, month, and day of the month, and displays the name of the day of the week. 
+ * Here are some sample runs:
+ * (see supplied image of the question) 
+ * 
+ * @author Muchi Dec 12, 2021
+ *
+ */
+public class WeekDay {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
