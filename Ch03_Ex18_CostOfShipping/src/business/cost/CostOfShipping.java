7f2006bec489037d633ef6e22bb554e34b438e75
Muchi Gora <muchigora@gmail.com>
2021-12-18 07:58:06 -0500

    First commit

---
+++ Ch03_Ex18_CostOfShipping/src/business/cost/CostOfShipping.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package business.cost;
+
+/**
+ * @author Muchi Dec 12th, 2021
+ *
+ */
+public class CostOfShipping {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
