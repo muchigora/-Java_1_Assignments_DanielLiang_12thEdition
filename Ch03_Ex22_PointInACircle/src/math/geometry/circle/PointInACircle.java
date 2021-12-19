e940de49b67bc3f30ffbf1cc9efa13cb3fc609a0
Muchi Gora <muchigora@gmail.com>
2021-12-19 15:05:06 -0500

    Created class

---
+++ Ch03_Ex22_PointInACircle/src/math/geometry/circle/PointInACircle.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package math.geometry.circle;
+
+/**
+ * Chapter 3 Exercise 22 Geometry: Point in a Circle?
+ * Write a program that prompts the user to enter a point (x, y) and checks whether the point is within 
+ * the circle centered at (0, 0) with radius 10. 
+ * For example, (4, 5) is inside the circle and (9, 9) is outside the circle, as shown in Figure 3.7a.
+ * (Hint: A point is in the circle if its distance to (0, 0) is less than or equal to 10. 
+ * The formula for computing the distance is sqrt((x2 - x1)^2 + (y2 - y1)^2). 
+ * Test your program to cover all cases.) Two sample runs are shown below(see attached image of question)
+ * 
+ * @author Muchi this 19th Day of December 2021
+ *
+ */
+public class PointInACircle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
