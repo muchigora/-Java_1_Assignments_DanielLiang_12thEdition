73e850e762a2df910d51fd615d9c2919f9867d9b
Muchi Gora <muchigora@gmail.com>
2021-12-25 02:12:15 -0500

    Final Commit

--- Ch03_Ex23_PointInARectangle/src/geometry/rectangle/coordinatepoint/Point.java
+++ Ch03_Ex23_PointInARectangle/src/geometry/rectangle/coordinatepoint/Point.java
@@ -3,6 +3,8 @@
  */
 package geometry.rectangle.coordinatepoint;
 
+import java.util.Scanner;
+
 /**
  * Ch03 Ex 23 Geometry: point in a rectangle?
  * Write a program that prompts the user to enter a point (x, y) and checks whether the point is within 
@@ -19,7 +21,26 @@ public class Point {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create scanner
+				Scanner input = new Scanner(System.in);
+				
+				//Prompt user for coordinates
+				System.out.println("Enter x-coordinate: ");
+				double x = input.nextDouble();
+				
+				System.out.println("Enter y-coordinate: ");
+				double y = input.nextDouble();
+				
+				//System.out.println("Point entered is: (" + x + "," + y +")");
+				
+				//determine if distance of point is within rectangle
+				double xNew = Math.sqrt(Math.pow(x, 2));
+				double yNew = Math.sqrt(Math.pow(y, 2));
+				
+				if (xNew <= 5.0 && yNew <= 2.5)
+					System.out.println("Point ("+x+","+y+") is inside the rectangle");
+				else
+					System.out.println("Point ("+x+","+y+") is outside the rectangle");
 
 	}
 
