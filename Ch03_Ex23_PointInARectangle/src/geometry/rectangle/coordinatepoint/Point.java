c16b8d86944151594d9a04aba7a60ecf155c8075
Muchi Gora <muchigora@gmail.com>
2021-12-24 23:19:43 -0500

    Commit changes - created class

---
+++ Ch03_Ex23_PointInARectangle/src/geometry/rectangle/coordinatepoint/Point.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package geometry.rectangle.coordinatepoint;
+
+/**
+ * Ch03 Ex 23 Geometry: point in a rectangle?
+ * Write a program that prompts the user to enter a point (x, y) and checks whether the point is within 
+ * the rectangle centered at (0, 0) with width 10 and height 5. For example, (2, 2) is inside the rectangle 
+ * and (6, 4) is outside the rectangle, as shown in Figure 3.7b. 
+ * (Hint: A point is in the rectangle if its horizontal distance to (0, 0) is less than or equal to 10 / 2 and 
+ * its vertical distance to (0, 0) is less than or equal to 5.0 / 2. Test your program to cover all cases.) 
+ * Here are two sample runs:
+ * 
+ * @author Muchi 12/24/2021
+ *
+ */
+public class Point {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
