696b7bb08c6603985ef80b0f3da3b1d923203966
Muchi Gora <muchigora@gmail.com>
2021-12-25 02:24:32 -0500

    Commit changes - Add class

---
+++ Ch03_Ex24_PickACard/src/games/PickACard.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package games;
+
+/**
+ * Ch03 Ex24: Game: pick a card
+ * Write a program that simulates picking a card from a deck of 52 cards. 
+ * Your program should display the rank (Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King) 
+ * and suit (Clubs, Diamonds, Hearts, Spades) of the card.
+ * Here is a sample run of the program
+ * 
+ * @author Muchi Dec 25th, of 2021
+ */
+public class PickACard {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
