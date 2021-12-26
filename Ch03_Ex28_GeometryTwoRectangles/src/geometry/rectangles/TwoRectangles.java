efa839ada62b30baa1747d3b3e370a75dd3c4546
Muchi Gora <muchigora@gmail.com>
2021-12-26 03:11:16 -0500

    Commit Changes - add class

---
+++ Ch03_Ex28_GeometryTwoRectangles/src/geometry/rectangles/TwoRectangles.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package geometry.rectangles;
+
+/**
+ * Chapter 3 Excercise 28: Geometry: two rectangle 
+ * Write a program that prompts the user to enter the center x-, y-coordinates, 
+ * width, and height of two rectangles and determines whether the second rectangle is inside 
+ * the first or overlaps with the first, as shown in Figure 3.9. 
+ * Test your program to cover all cases
+ * 
+ * @author Muchi
+ *
+ */
+public class TwoRectangles {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
