a8bcc01ce0389bec1d77198f5885fc85524379c5
Muchi Gora <muchigora@gmail.com>
2021-12-26 05:36:21 -0500

    Commit Changes - Add class

---
+++ Ch03_Ex30_CurrentTime/src/time/CurrentTime.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package time;
+
+/**
+ * Chapter 03 Excercise 30: Current time
+ * Revise Programming Exercise 2.8 to display the hour using a 12-hour clock. 
+ * Here is a sample run: see image attached
+ * @author Muchi on Dec 26th, 2021
+ *
+ */
+public class CurrentTime {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
