bdb077cb72d949ad398d556bec6541115db98e62
Muchi Gora <muchigora@gmail.com>
2021-12-26 04:23:52 -0500

    Commit Changes - add class

---
+++ Ch03_Ex29_GeometryTwoCircles/src/geometry/circle/TwoCircles.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package geometry.circle;
+
+/**
+ * Ch03 Ex 29: Geometry: two circles
+ * Write a program that prompts the user to enter the center coordinates and radii of two circles 
+ * and determines whether the second circle is inside the first or overlaps with the first, as shown 
+ * in Figure 3.10. (Hint: circle2 is inside circle1 if the distance between the two 
+ * centers <= r1 − r2 and circle2 overlaps circle1 if the distance between the two centers 
+ * >=r1 + r2. 
+ * Test your program to cover all cases. (See question image attached)
+ * 
+ * @author Muchi on  Dec 26th, 2021
+ *
+ */
+public class TwoCircles {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
