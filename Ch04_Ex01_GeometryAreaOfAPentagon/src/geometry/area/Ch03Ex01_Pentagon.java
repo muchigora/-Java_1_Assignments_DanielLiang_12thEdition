a37560e8e8e00489accf4c345704d8687c6d6fcc
Muchi Gora <muchigora@gmail.com>
2021-12-29 21:45:27 -0500

    Add class

---
+++ Ch04_Ex01_GeometryAreaOfAPentagon/src/geometry/area/Ch03Ex01_Pentagon.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package geometry.area;
+
+/**
+ * Chapter 4 Exercise 1: Geometry: area of a pentagon
+ * Write a program that prompts the user to enter the length from the center of a pentagon to a 
+ * vertex and computes the area of the  pentagon, as shown in the following figure (see question image)
+
+ * @author Muchi compiled on Dec 29th of 2021
+ *
+ */
+public class Ch03Ex01_Pentagon {
+
+	
+	public static void main(String[] args) {
+		// Create the scanner
+		
+		// Prompt user of radius / area from center of pentagon to vertex
+		
+		//compute length of a side
+		
+		
+
+	}
+
+}
