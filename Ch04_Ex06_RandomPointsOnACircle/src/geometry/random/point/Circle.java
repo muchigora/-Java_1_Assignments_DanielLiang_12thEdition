5ca99b1c024baf94814ee787c659508df3b10fa2
Muchi Gora <muchigora@gmail.com>
2021-12-31 03:38:19 -0500

    Initial Commit

---
+++ Ch04_Ex06_RandomPointsOnACircle/src/geometry/random/point/Circle.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package geometry.random.point;
+
+/**
+ * Chapter 04 Exercise 6 Random points on a circle
+ * Write a program that generates three random points on a circle centered at (0, 0) with radius 40 
+ * and displays three angles in a triangle formed by these three points, as shown in Figure 4.4a. 
+ * Display the angles in degrees.
+ * (a) A triangle is formed from three random points on the circle. 
+ * (b) A random point on the circle can be generated using a random angle a. 
+ * (c) A pentagon is centered at (0, 0) with one point at the 0 o’clock position.
+ * 
+ * @author Muchi compiled Dec 31st, 2021
+ */
+public class Circle {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
