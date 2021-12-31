e09ebdf5b13b4e0d071a756b5f93b2389e9c770f
Muchi Gora <muchigora@gmail.com>
2021-12-31 03:49:43 -0500

    Initial Commit

---
+++ Ch04_Ex07_CornerPointCoodinates/src/geometry/circle/CornerPointCoordinates.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package geometry.circle;
+
+/**
+ * Chapter 4 Exercise 7: Corner point coordinates
+ * Suppose a pentagon is centered at (0, 0) with one point at the 0 o’clock position, as shown in 
+ * Figure 4.4c. Write a program that prompts the user to enter the radius of the bounding circle of 
+ * a pentagon and displays the coordinates of the five corner points on the pentagon from p1 to p5 
+ * in this order. 
+ * Use console format to display two digits after the decimal point. 
+ * 
+ * @author Muchi
+ *
+ */
+public class CornerPointCoordinates {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
