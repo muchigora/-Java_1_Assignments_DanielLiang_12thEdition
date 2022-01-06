7c8d582152b2009b1efd3c26941291168dbefaf1
Muchi Gora <muchigora@gmail.com>
2022-01-06 01:14:07 -0500

    Final Commit

--- Ch04_Ex07_CornerPointCoodinates/src/geometry/circle/CornerPointCoordinates.java
+++ Ch04_Ex07_CornerPointCoodinates/src/geometry/circle/CornerPointCoordinates.java
@@ -3,6 +3,8 @@
  */
 package geometry.circle;
 
+import java.util.Scanner;
+
 /**
  * Chapter 4 Exercise 7: Corner point coordinates
  * Suppose a pentagon is centered at (0, 0) with one point at the 0 o’clock position, as shown in 
@@ -11,14 +13,28 @@ package geometry.circle;
  * in this order. 
  * Use console format to display two digits after the decimal point. 
  * 
- * @author Muchi
+ * @author Muchi Jan 3, 2022
  *
  */
 public class CornerPointCoordinates {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create Scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Prompt User to enter radius of the pentagon
+		System.out.println("Enter radius of your Pentagon: ");
+		double r = input.nextDouble();
+		
+		//Determine length of each side
+		double side = 2 * r * Math.sin(Math.PI/5);
+		
+		
+		//Determine each point
+		//Point One
+		double x1 = 0;
+		System.out.println("P2: ("+x1+","+r+")");
 
 	}
 
